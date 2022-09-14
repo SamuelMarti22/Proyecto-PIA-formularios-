4fa0515ef2fa14c45b9e2462005b447fd22c0cc5
SamuelMarti22 <samuelmartinezarteaga22@gmail.com>
2022-09-14 16:28:14 -0500

    paginas listas (solo montar info)

---
+++ SISED (prototipo)/script/Objetos (informacion)/plantas.js
@@ -0,0 +1,23 @@
+export const objetoPlantas = [
+    {
+        'id': 1,
+        'clasif':'Peligroso',
+        'nombre':'Puma (Puma concolor)',
+        'info':'El puma es terrestre, pero puede trepar árboles para descansar, ocultar sus presas o esconderse de los perros y de los cazadores. Son solitarios, salvo durante la época de cortejo o en el caso de hembras con sus crías.',
+        'img':'./assests/Fotos_animales/Puma.jpg'
+    },
+    {
+        'id': 2,
+        'clasif':'Sano',
+        'nombre':'Guagua (Cuniculus paca)',
+        'info':'Las guartinajas o guaguas habitan en general cerca de fuentes hídricas, cerca de ríos, lagunas, en ecosistemas de bosques tropicales. Esta especie se caracteriza por poseer hábitos Nocturno y crepusculares. También es una especie generalmente frugívora y muchas ocasiones herbívora.',
+        'img':'./assests/Fotos_animales/Guagua.jpg'
+    },
+    {
+        'id': 3,
+        'clasif':'Precaución',
+        'nombre':'Ñeque (Dasyprocta punctata)',
+        'info':'Roedor de tamaño mediano con orejas desnudas pequeñas y cola apenas visible. La coloración es parda amarillenta pálida jaspeada de gris, con diversas tonalidades; los pelos de la grupa son negros con bandas blancas o amarillentas. Flancos y extremidades pardas.',
+        'img':'./assests/Fotos_animales/Ñeque.jpg'
+    },
+]
---
+++ SISED (prototipo)/script/script_hongos.js
@@ -0,0 +1,30 @@
+/* ------------ INFORMACION SOBRE LAS ESPECIES (TARJETAS) ---------------- */
+import { objetoHongos } from "./Objetos (informacion)/hongos.js";
+const items = document.getElementById('items');
+const templateCard = document.getElementById('template-card').content;
+const fragment = document.createDocumentFragment();
+
+document.addEventListener('DOMContentLoaded',()=>{
+    loadData(objetoAnimales)
+})
+const loadData = objetoAnimales => {
+    objetoAnimales.forEach(animales => {
+        const { id,clasif, nombre, info, img} = animales
+        let color = ''
+        templateCard.querySelector('h5').textContent = nombre
+        if (clasif == 'Peligroso'){
+            color ='color: red'
+        } else if (clasif =='Sano'){
+            color ='color: green'
+        } else {
+            color ='color: yellow'
+        }
+        templateCard.querySelector('.text-muted').textContent = clasif
+        templateCard.querySelector('.text-muted').setAttribute('style',color)
+        templateCard.querySelector('img').setAttribute('src',img)
+        templateCard.querySelector('.card-text').textContent = info
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
+}
