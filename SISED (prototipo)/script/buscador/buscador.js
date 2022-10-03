f69a444a7123ba0ca538149a6f35682f59c4b8a7
SamuelMarti22 <samuelmartinezarteaga22@gmail.com>
2022-10-03 14:06:27 -0500

    algo

--- SISED (prototipo)/script/buscador/buscador.js
+++ SISED (prototipo)/script/buscador/buscador.js
@@ -1,13 +1,16 @@
 const d = document;
-import  function objetoAnimales(input,selector){
+import { objetoAnimales } from "../Objetos (informacion)/animales.js";
+function BuscadorAnimales(input){
     d.addEventListener("indice",e=>{
         if(e.target.matchest(input)){
             console.log(e.key);
-            d.querySelectorAll(selector).forEach(el=>el.textContent.toLowerCase()
+            d.querySelectorAll("#template-card").forEach(e=>e.textContent.toLowerCase()
             .includes(e.target.value)
-            ?el.clasList.remove("filter")
-            :el.clasList.add("filter"))
+            ?e.setAttribute(id,"template-card filter")
+            :e.setAttribute(id,"template-card"))
         }
     })
-    console.log(objetoAnimales)
+    console.log(input)
 }
+
+d.addEventListener('keyup',BuscadorAnimales)
