a0e3438f8c8c6dbc37de7ba9d01fb1ea82615769
lafamiliadeami <danielalopezlondono@iesantaelena.edu.co>
2022-10-28 16:18:19 -0500

    Plantas

--- SISED (prototipo)/script/Objetos (informacion)/plantas.js
+++ SISED (prototipo)/script/Objetos (informacion)/plantas.js
@@ -8,16 +8,38 @@ export const objetoPlantas = [
     },
     {
         'id': 2,
-        'clasif':'Tóxico',
-        'nombre':'Guagua (Cuniculus paca)',
-        'info':'Las guartinajas o guaguas habitan en general cerca de fuentes hídricas, cerca de ríos, lagunas, en ecosistemas de bosques tropicales. Esta especie se caracteriza por poseer hábitos Nocturno y crepusculares. También es una especie generalmente frugívora y muchas ocasiones herbívora.',
-        'img':'./assests/Fotos_animales/Guagua.jpg'
+        'clasif':'No perjudicial',
+        'nombre':'Bomaria (Bomarea setacea)',
+        'info':'Planta trepadora de tallos pubescentes. Hojas simples, alternas, ovadas con pubescencia en el envés. Inflorescencia en umbela simple; flores rojas y amarillas con 6 tépalos de igual tamaño. Fruto en cápsula, esférico, con semillas rojas y anaranjadas.',
+        'img':'./assests/Fotos_plantas/Bomarea_Southern_Ecuador.jpg'
     },
     {
         'id': 3,
-        'clasif':'Precaución',
-        'nombre':'Ñeque (Dasyprocta punctata)',
-        'info':'Roedor de tamaño mediano con orejas desnudas pequeñas y cola apenas visible. La coloración es parda amarillenta pálida jaspeada de gris, con diversas tonalidades; los pelos de la grupa son negros con bandas blancas o amarillentas. Flancos y extremidades pardas.',
-        'img':'./assests/Fotos_animales/Ñeque.jpg'
+        'clasif':'Alérgico para algunos',
+        'nombre':'Paico (Chenopodium anthelminticum)',
+        'info':'Planta herbácea de 40 centímetros a 1 metro de altura. Tallos erectos, simples o ramificados. Hojas lanceoladas a ovadas de 2 a 7 centímetros de largo y de 0.5 a 2.5 centímetros de ancho; hojas inferiores pecioladas y superiores sésiles; glabras o pubescentes, con abundantes glándulas. Inflorescencias en espigas densas o discontinuas. Fruto seco y dehiscente. Semilla de color café rojizo a negra.',
+        'img':'./assests/Fotos_plantas/Paico (Chenopodium anthelminticum).jpg'
     },
+    {
+        'id': 4,
+        'clasif':'No perjudicial',
+        'nombre':'Cartucho blanco (Zantedeschia aethiopica)',
+        'info':'Planta herbácea perenne, entre 60 centímetros a 1 metro de altura. Posee un rizoma oblongo y grueso del que surgen raíces de hasta 15 centímetros de largo. Hojas de color verde brillante basales y largamente pecioladas. Inflorescencias erectas, de 4 a 18 centímetros de largo y envueltas por unas brácteas modificadas blancas de forma acampanada. En las variedades pueden ser de diversos colores.',
+        'img':'./assests/Fotos_plantas/Cartucho blanco (Zantedeschia aethiopica).jpg'
+    },
+    {
+        'id': 5,
+        'clasif':'No perjudicial',
+        'nombre':'Tritoma (Kniphofia uvaria)',
+        'info':'Es una planta herbácea perennifolia con las hojas ensiforme-acuminadas, ligeramente glaucas, moderadamente firmes en textura, de 30 a 60 cm de largo, 2,50 cm de ancho, escabroso el margen, con aguda quilla, y con unas 30-40 venas verticales. La inflorescencia con pedúnculo, del tamaño de las hojas, en forma de racimo denso, con brácteas ovadas, obtusas, el perianto cilíndrico. El fruto en forma de cápsula ovoide.',
+        'img':'./assests/Fotos_plantas/Tritoma (Kniphofia uvaria).jpg'
+    },
+    {
+        'id': 6,
+        'clasif':'Alérgico para algunos',
+        'nombre':'Manzanillón (Erigeron karvinskianus)',
+        'info':'Esta planta trepadora o amacollada , es útil como tapizante informal y en climas suaves florece con profusion todo el año. Las pequeñas flores, de 25 mm de ancho son blancas al abrirse y adoptan diversos tonos rosados y rojo burdeos. Crece hasta unos 38 cm de alto, se extiende indefinidamente y tiene tallos laxos y estrechas hojas vellosas, a menudo lobuladas. Puede ser bastante invasiva en climas suaves.',
+        'img':'./assests/Fotos_plantas/Manzanillón (Erigeron karvinskianus).jpg'
+    },
+    
 ]
