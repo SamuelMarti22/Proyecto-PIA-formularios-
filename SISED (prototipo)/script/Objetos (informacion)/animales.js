90f9f865d511b369510c8bc62a60d8a45628e4dc
SamuelMarti22 <samuelmartinezarteaga22@gmail.com>
2022-10-28 16:17:40 -0500

    cambios grandecitos

--- SISED (prototipo)/script/Objetos (informacion)/animales.js
+++ SISED (prototipo)/script/Objetos (informacion)/animales.js
@@ -3,21 +3,42 @@ export const objetoAnimales = [
         'id': 1,
         'clasif':'Peligroso',
         'nombre':'Puma (Puma concolor)',
-        'info':'El puma es terrestre, pero puede trepar árboles para descansar, ocultar sus presas o esconderse de los perros y de los cazadores. Son solitarios, salvo durante la época de cortejo o en el caso de hembras con sus crías.',
+        'info':'El puma es terrestre, pero puede trepar árboles para descansar, ocultar sus presas o esconderse de los perros y de los cazadores. Son solitarios, salvo durante la época de cortejo o en el caso de hembras con sus crías. En el caso de encontrarse con uno no se acerque al puma, mantenga la calma, mire hacia el puma y levante los brazos para buscar aparentar ser lo más grande posible. Si está acompañado, subase en la espalda de su acompañante para aparentar mayor tamaño. La idea es convencer al puma que usted no es una presa y que puede ser un peligro él.',
         'img':'./assests/Fotos_animales/Puma.jpg'
     },
     {
         'id': 2,
         'clasif':'No perjudicial',
         'nombre':'Guagua (Cuniculus paca)',
-        'info':'Las guartinajas o guaguas habitan en general cerca de fuentes hídricas, cerca de ríos, lagunas, en ecosistemas de bosques tropicales. Esta especie se caracteriza por poseer hábitos Nocturno y crepusculares. También es una especie generalmente frugívora y muchas ocasiones herbívora.',
+        'info':'Las guartinajas o guaguas habitan en general cerca de fuentes hídricas, cerca de ríos, lagunas, en ecosistemas de bosques tropicales. Esta especie se caracteriza por poseer hábitos Nocturno y crepusculares. También es una especie generalmente frugívora y muchas ocasiones herbívora. Al ser pequeños roedores, frugíforos y hervivoros, no tenemos ningún tipo de peligro, pero es mejor dejarlos quietos.',
         'img':'./assests/Fotos_animales/Guagua.jpg'
     },
     {
         'id': 3,
         'clasif':'Precaución',
         'nombre':'Ñeque (Dasyprocta punctata)',
-        'info':'Roedor de tamaño mediano con orejas desnudas pequeñas y cola apenas visible. La coloración es parda amarillenta pálida jaspeada de gris, con diversas tonalidades; los pelos de la grupa son negros con bandas blancas o amarillentas. Flancos y extremidades pardas.',
+        'info':'Roedor de tamaño mediano con orejas desnudas pequeñas y cola apenas visible. La coloración es parda amarillenta pálida jaspeada de gris, con diversas tonalidades; Tiene la costumbre de sepultar semillas para tenerlas de reserva en épocas de escasez de alimentos, Sin embargo, en muchos casos no las encuentra de nuevo y estas germinan. Esto lo convierte en un importante dispersor de semillas para árboles y palmas.',
         'img':'./assests/Fotos_animales/Ñeque.jpg'
+    },
+    {
+        'id': 4,
+        'clasif':'No perjudicial',
+        'nombre':'Armadillo (Dasypus novemcinctus)',
+        'info':'Se caracterizan por poseer un caparazón dorsal formado por placas yuxtapuestas, ordenadas por lo general en filas transversales, con cola bastante larga y extremidades cortas. No representan daño al ser humano, por el contrario, lo evitan y se hacen bolita cuando se sienten amenazados, es mejor no molestarlos',
+        'img':'./assests/Fotos_animales/armadillo.webp'
+    },
+    {
+        'id': 5,
+        'clasif':'No perjudicial',
+        'nombre':'Barranquero (Momotus aequatorialis)',
+        'info':'Tiene una cabeza voluminosa, casi todo su cuerpo es de color verde con alas y cola azules, en su rostro tiene máscara negra y una corona azul brillante con centro negro, su pico es largo, grueso y decurvado. Suelen ser diurnos, y hacen sus madrigueras en la tierra. No suponen ningun riesgo ni peligro, por lo que lo mejor es no hacerles daño ni asustarlos.',
+        'img':'./assests/Fotos_animales/Barranquero.jpg'
+    },
+    {
+        'id': 6,
+        'clasif':'No perjudicial',
+        'nombre':'Barranquero (Momotus aequatorialis)',
+        'info':'Se caracterizan por poseer un caparazón dorsal formado por placas yuxtapuestas, ordenadas por lo general en filas transversales, con cola bastante larga y extremidades cortas. No representan daño al ser humano, por el contrario, lo evitan y se hacen bolita cuando se sienten amenazados, es mejor no molestarlos',
+        'img':'./assests/Fotos_animales/armadillo.webp'
     },
 ]
