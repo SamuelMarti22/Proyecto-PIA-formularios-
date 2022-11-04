1b27b93891eb2a9b0a52a6c32db9210d67841fe1
SamuelMarti22 <samuelmartinezarteaga22@gmail.com>
2022-11-04 13:53:18 -0500

    hi

--- SISED (prototipo)/script/buscador/buscador.js
+++ SISED (prototipo)/script/buscador/buscador.js
@@ -1,11 +1,7 @@
-import { objetoAnimales } from "./Objetos (informacion)/animales.js";
-
-console.log(objetoAnimales)
-
 document.addEventListener('keyup', e =>{
     if(e.target.matches("#buscador")){
-        document.querySelectorAll(objetoAnimales).forEach(elemento => {
-            elemento.name.toLowerCase().includes(e.target.value.toLowerCase)
+        document.querySelectorAll(container).forEach(elemento => {
+            elemento.querySelector('.card-title').textContent.toLowerCase().includes(e.target.value.toLowerCase)
             ?elemento.classList.remove("filtro")
             :elemento.classList.add("filtro")
         })
