d8acde0638ec3e329526c058ae4c7c0c0f62814e
SamuelMarti22 <samuelmartinezarteaga22@gmail.com>
2022-11-08 23:05:14 -0500

    Hongos y otras cositas

--- SISED (prototipo)/script/Objetos (informacion)/hongos.js
+++ SISED (prototipo)/script/Objetos (informacion)/hongos.js
@@ -2,7 +2,7 @@ export const objetoHongos = [
     {
         'id': 1,
         'clasif':'Tóxico',
-        'nombre':'Clathrus archeri (dedos del diablo)',
+        'nombre':'Dedos del diablo (Clathrus archeri)',
         'info':'Clathrus archeri, también conocido como «dedos del diablo»,​ «seta calamar» o «estrella roja»,​ es un hongo basidiomiceto de la familia Phallaceae​ australiano o neozelandés, fue descubierto por primera vez en Europa en 1914 en el distrito francés de Saint-Dié-des-Vosges.',
         'img':'./assests/fotos_hongos/diablo.jpg'
     },
@@ -16,8 +16,29 @@ export const objetoHongos = [
     {
         'id': 3,
         'clasif':'Venenoso',
-        'nombre':'Amanita muscaria (matamoscas)',
+        'nombre':'Matamoscas (Amanita muscaria)',
         'info':'Amanita muscaria, también conocido como matamoscas o falsa oronja, entre otros nombres, ​​ es un hongo basidiomiceto muy común y popular, considerado venenoso y enteógeno, de distribución cosmopolita del orden Agaricales.​​',
         'img':'./assests/fotos_hongos/falsa-oronja.jpeg'
+    },
+    {
+        'id': 4,
+        'clasif':'Venenoso',
+        'nombre':'Hongo de la muerte (Amanita phalloides)',
+        'info':'Amanita phalloides, también conocida en español como oronja verde, ​ canaleja, ​ hongo de la muerte, ​ oronja mortal y cicuta verde, es una especie de hongo micorrizógeno venenosa muy parecida a algunas que son comestibles, por lo que se han dado casos de envenenamiento accidental.​​',
+        'img':'./assests/fotos_hongos/Amanita phalloides.jpeg'
+    },
+    {
+        'id': 5,
+        'clasif':'Tóxico',
+        'nombre':'Paxillus involutus',
+        'info':'Paxillus involutus, es un hongo tóxico, basidiomiceto, del orden Boletales, de la familia Paxillaceae. Se creía que era comestible, pero ahora se sabe que destruye los glóbulos rojos cuando se consume con frecuencia.​​',
+        'img':'./assests/fotos_hongos/Paxillus involutus.jpg'
+    },
+    {
+        'id': 6,
+        'clasif':'Tóxico',
+        'nombre':'Lepiota cristata',
+        'info':'Lepiota cristata, o lepiota maloliente, ​ es hongo basidiomiceto de la familia Agaricaceae, ​​ que habita tanto en zonas de arbustos, hierba o bosques. Puede producir serios trastornos gastrointestinales. Su olor y sabor desagradables, desaconsejan su consumo.​​',
+        'img':'./assests/fotos_hongos/Lepiota cristata.jpg'
     },
 ]
